a25c825820d9ca2467d87c7e97fd36f9567682f5
Ben Neiswander <ben.neiswander@gmail.com>
2017-04-21 10:14:05 -0500

    adds greeting

--- app/components/Jokes.jsx
+++ app/components/Jokes.jsx
@@ -20,6 +20,7 @@ export default class BonesJokes extends Component {
     const {joke, answered} = this.state
     return (
       <div onClick={answered ? this.nextJoke : this.answer}>
+        <h1>Hello!</h1>
         <h1>{joke.q}</h1>
         {answered && <h2>{joke.a}</h2>}
         <cite>~xoxo, bones</cite>
